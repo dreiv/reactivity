1e40acc856daaf63456a63a453154964dc7a4aef
Andrei Voicu <dreivoicu@gmail.com>
2020-08-27 12:39:13 +0300

    semi reactive

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 let price = 5
 let quantity = 2
 let total = 0
-let target = null;
+let target = null
+let storage = []
 
 target = () => {
   total = price * quantity
 }
 
+function record() {
+  storage.push(target)
+}
+
+function replay() {
+  storage.forEach(run => run())
+}
+
 record()
 target()
-replay()
 
 console.log(`total is ${total}`)
 
 price = 20
+replay()
 
 console.log(`total is ${total}`)
